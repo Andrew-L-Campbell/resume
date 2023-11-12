c26af3fc4f3db88a1e0bb341663779e019b865ab
Andrew-L-Campbell <83891416+Andrew-L-Campbell@users.noreply.github.com>
2023-11-12 10:07:21 -0700

    added images to react project

--- app.js
+++ app.js
@@ -24,3 +24,54 @@ menu_item.forEach((item)=>{
         mobile_menu.classList.toggle('active');
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const sliderWrapper = document.getElementById('slider-wrapper');
+    const prevBtn = document.getElementById('prev-btn');
+    const nextBtn = document.getElementById('next-btn');
+
+    // Replace these image URLs with your own
+    const imageUrls = [
+        './img/ProjectPart1.png',
+        './img/ProjectPart2.png',
+        './img/ProjectPart3.png',
+        './img/ProjectPart4.png',
+        './img/ProjectPart5.png',
+        './img/ProjectPart6.png'
+    ];
+
+    imageUrls.forEach((imageUrl) => {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        sliderWrapper.appendChild(img);
+    });
+
+    let currentIndex = 0;
+
+    function showImage(index) {
+        const transformValue = -index * 100 + '%';
+        sliderWrapper.style.transform = 'translateX(' + transformValue + ')';
+    }
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        showImage(currentIndex);
+    }
+
+    function showPrevImage() {
+        currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+        showImage(currentIndex);
+    }
+
+    // Add event listeners for navigation (next and previous)
+    nextBtn.addEventListener('click', showNextImage);
+    prevBtn.addEventListener('click', showPrevImage);
+
+    // Add event listeners for keyboard navigation (left and right arrow keys)
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'ArrowRight') {
+            showNextImage();
+        } else if (event.key === 'ArrowLeft') {
+            showPrevImage();
+        }
+    });
+});
